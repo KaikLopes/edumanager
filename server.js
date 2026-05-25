cc059298072856e5b4491d2363bd6d58a477d591
Ruan Vituriano Claudino <ruan.claudino@cwi.com.br>
2026-05-25 14:32:21 -0300

    feat: adiciona metodos de adicionar aluno e remover aluno

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ app.use(cors());
 app.use(express.json());
 
 let bancoAlunos = [];
+
+
+// ---------------------------------------------------------
+// RESPONSÁVEL: Ruan Vituriano (US3 - Matricular Aluno)
+// Rota para receber os dados do Frontend e salvar na memória
+// ---------------------------------------------------------
+app.post('/alunos', (req, res) => {
+    const novoAluno = req.body;
+    bancoAlunos.push(novoAluno);
+    res.status(201).json({ mensagem: "Aluno matriculado com sucesso!" });
+});
+
